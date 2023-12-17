45cd8b421f994aa21dd406d78dc28c658b8a8845
Reza Afshinmehr <88842899+dr-afshinmehr@users.noreply.github.com>
2023-12-17 18:34:12 +0330

    create an simple digital clock

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from time import strftime
+
+def time():
+    string = strftime('%I:%M:%S %p')
+    label.config(text = string)
+    label.after(1000, time)
+
+root = tk.Tk()
+root.title("Beautiful Digital Clock")
+
+label = tk.Label(root, font=('calibri', 40, 'bold'), background='gray', foreground='white')
+label.pack(anchor = 'center')
+
+time()
+root.mainloop()
